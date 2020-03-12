8f8faaf81470f0dc8e43921cca1404e8c8461f9c
Cloud User <centos@ip-172-31-23-102.eu-west-2.compute.internal>
2020-03-12 20:28:29 +0000

    modified vpc_module

--- VPC/backend.tf
+++ VPC/backend.tf
@@ -1,7 +1,7 @@
 terraform {
 backend "s3" {
 bucket = "terraform-class-sezgin"
-key = "tower/us-east-1/tools/oregon/tower.tfstate"
+key = "tower/us-east-1/tools/virginia/tower.tfstate"
 region = "us-east-1"
   }
 }
