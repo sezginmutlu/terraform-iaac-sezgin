8ed1add45cb33777c887a329073aee2f890f3ef2
Cloud User <centos@ip-172-31-23-102.eu-west-2.compute.internal>
2020-03-13 00:45:18 +0000

    updated

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -2,6 +2,9 @@ provider "aws" {
   region = "us-west-2"
 }
 
+
+# Gets Ubuntu AMI
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -18,6 +21,48 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-output "AMI_ID" {
+output "UBUNTU_AMI_ID" {
   value       = "${data.aws_ami.ubuntu.id}"
 }
+
+# Gets Centos AMI
+
+data "aws_ami" "centos" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["CentOS Linux 7 x86_64 HVM EBS *"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["679593333241"] # Canonical
+}
+
+output "CENTOS_AMI_ID" {
+  value       = "${data.aws_ami.ubuntu.id}"
+}
+
+
+resource "aws_key_pair" "provisioner" {
+  key_name   = "provisioner-key"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+}
+
+
+
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+  key_name      = "${aws_key_pair.provisioner.key_name}"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
