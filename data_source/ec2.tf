1306972b71548d3b41eb752cb1832df167279bf0
Cloud User <centos@ip-172-31-23-102.eu-west-2.compute.internal>
2020-03-13 01:20:30 +0000

    updated ec2.tf

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -61,6 +61,19 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
   key_name      = "${aws_key_pair.provisioner.key_name}"
 
+
+  provisioner "file" {
+    connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    private_key = "${file("~/.ssh/id_rsa")}"
+    host     = "${self.public_ip}"
+   }
+    source      = "test"
+    destination = "/tmp/"
+  
+ }
+
   tags = {
     Name = "HelloWorld"
   }
